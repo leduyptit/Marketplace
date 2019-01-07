02d78a49d7ca807ae5e472879d35e3e1f975d3d4
DuyLK <duylk@arrowhitech.net>
2019-01-07 19:42:52 +0700

    Desktop Commit 07.01.2017

--- Block/Profile/Index.php
+++ Block/Profile/Index.php
@@ -3,5 +3,8 @@ namespace MGS\Marketplace\Block\Profile;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
-
+	public function getPostAction()
+	{
+		return $this->getUrl('mgs_marketplace/profile/index');
+	}
 }
